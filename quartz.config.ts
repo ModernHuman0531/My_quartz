62a5d2664d3aa683424c0464537dbec150236434
ModernHuman0531 <dunkun130897441@gmail.com>
2025-08-02 19:00:15 +0800

    Quartz sync: Aug 2, 2025, 7:00 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "/ModernHuman0531.github.io/My_quartz/",
+    baseUrl: "ModernHuman0531.github.io/My_quartz/",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
