a3f73a07c3f41b721c876b7f8783a9fc487378e5
ModernHuman0531 <dunkun130897441@gmail.com>
2025-08-02 19:28:43 +0800

    Quartz sync: Aug 2, 2025, 7:28 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -17,7 +17,6 @@ const config: QuartzConfig = {
     },
     locale: "en-US",
     baseUrl: "/My_quartz/",
-    verbose: true,
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
@@ -70,7 +69,7 @@ const config: QuartzConfig = {
       Plugin.ObsidianFlavoredMarkdown({ enableInHtmlEmbed: false }),
       Plugin.GitHubFlavoredMarkdown(),
       Plugin.TableOfContents(),
-      Plugin.CrawlLinks({ markdownLinkResolution: "shortest" }),
+      Plugin.CrawlLinks({ markdownLinkResolution: "shortest", recursive: true }),
       Plugin.Description(),
       Plugin.Latex({ renderEngine: "katex" }),
     ],
